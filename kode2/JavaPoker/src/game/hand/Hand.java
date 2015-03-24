b3830f28074b340e69597839e6efef8ce4308981
Nicolai Holst <nicolaiholst@live.dk>
2015-03-24 23:15:08 +0100

    f.eks. tirsdags arbejde

--- kode2/JavaPoker/src/game/hand/Hand.java
+++ kode2/JavaPoker/src/game/hand/Hand.java
@@ -3,84 +3,48 @@ package game.hand;
 import game.cards.Card;
 
 import java.util.ArrayList;
-<<<<<<< HEAD
-import java.util.Collections;
-import java.math.BigDecimal;
-import java.lang.Math;
+import java.util.List;
 
 public class Hand implements Comparable<Hand> {
 	private static ArrayList<Card> cards = new ArrayList<Card>();
-	private static ArrayList<Card> cards2 = new ArrayList<Card>();
-	private static ArrayList<Card> cc = new ArrayList<Card>();
-	private Card[] optimalHand;
+	private ArrayList<Card> optimalHand = new ArrayList<Card>();
 	private Rank rank;
 	private int[] subRanks;
 	private int[] suitDis = new int[4];
 	private int[] rankDis = new int[13];
-=======
-import java.util.List;
 
-public class Hand implements Comparable<Hand> {
-	private ArrayList<Card> cards;
-	private Card[] optimalHand;
-	private Rank rank;
-	private int[] subRanks;
->>>>>>> 22e2043c1d4916581afce0fd4929e6355776cf70
+	public static void main(String[] args) {
+		ArrayList<Card> cc = new ArrayList<Card>();
+		cc.add(Card.CA);
+		cc.add(Card.CQ);
+		cc.add(Card.S3);
+		cc.add(Card.CK);
+		cc.add(Card.D5);
 
-	public Hand(ArrayList<Card> cards) {
-		this.cards = cards;
-		evaluate();
-		System.out.println(findStraightFlush());
+		ArrayList<Card> handCards = new ArrayList<Card>();
+		handCards.add(Card.CJ);
+		handCards.add(Card.C10);
+
+		Hand hand = new Hand(handCards, cc);
+		System.out.println(hand.getRank());
+		System.out.println(hand.getAllCards());
+		System.out.println(hand.getoptimalCards());
 	}
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
-		cc.add(Card.CA);
-		cc.add(Card.C8);
-		cc.add(Card.S3);
-		cc.add(Card.C4);
-		cc.add(Card.S5);
-
-		cards.add(Card.C7);
-		cards.add(Card.C6);
-		cards.addAll(cc);
-
-		Hand hand = new Hand(cards);
-=======
 	public Hand(List<Card> hand, List<Card> community) {
 		cards.addAll(hand);
-		cards.addAll(community);
-		evaluate();
->>>>>>> 22e2043c1d4916581afce0fd4929e6355776cf70
+		if (community != null)
+			cards.addAll(community);
+		determineRank();
 	}
 
-	private void evaluate() {
-		// find the distributions of cards
-		for (Card c : cards) {
-			suitDis[c.getSuit()]++;
-			rankDis[c.getRank()]++;
-		}
-<<<<<<< HEAD
-		// determinRank(suitDis, rankDis);
+	public List<Card> getAllCards() {
+		return cards;
 	}
 
-	private int findStraight() {
-		int count = 0;
-
-		for (int i = rankDis.length-1; i >=0; i--) {
-			if (rankDis[i] > 0) {
-				count++;
-				if (count > 4) {
-					return i+4;
-				}
-			} else {
-				count = 0;
-			}
-		}
-		return 0;
+	public List<Card> getoptimalCards() {
+		return optimalHand;
 	}
-
-
 
 	private ArrayList<Integer> findNumOfDuplicates(int numOfDups) {
 
@@ -94,104 +58,133 @@ public class Hand implements Comparable<Hand> {
 		return dups;
 	}
 
-	private int isItFlush() {
+	private int maxDup() {
+		int max = 0;
+		for (int i = 0; i < rankDis.length; i++) {
+			if (rankDis[i] > max)
+				max = rankDis[i];
+		}
+		return max;
+	}
+
+	private boolean isFlush() {
+		return findSuit() != -1;
+	}
+
+	private int findStraightRank(int[] ranks) {
+		int count = 0;
+
+		for (int i = ranks.length - 1; i >= 0; i--) {
+			if (ranks[i] > 0) {
+				count++;
+				if (count > 4) {
+					return i + 4;
+				}
+			} else {
+				count = 0;
+			}
+		}
+		return -1;
+	}
+
+	private boolean isStraight() {
+
+		int count = 0;
+
+		for (int i = rankDis.length - 1; i >= 0; i--) {
+			if (rankDis[i] > 0) {
+				count++;
+				if (count > 4) {
+					return true;
+				}
+			} else {
+				count = 0;
+			}
+		}
+
+		if (count == 4)
+			return rankDis[Card.ACE] != 0;
+		return false;
+	}
+
+	private boolean findStraightFlush() {
+		int fs = findSuit();
+
+		int[] flushRanks = new int[13];
+		for (Card c : cards) {
+
+			if (c.getSuit() == fs) {
+				flushRanks[c.getRank()]++;
+			}
+		}
+		int straightRank = findStraightRank(flushRanks);
+
+		if (straightRank == -1) {
+			return false;
+		} else {
+			for (Card c : cards) {
+				if (c.getSuit() == fs && c.getRank() <= straightRank) {
+					if (c.getRank() > straightRank - 5) {
+						optimalHand.add(c);
+					} else if (straightRank == 3 && c.getRank() == 12) {
+						optimalHand.add(c);
+					}
+				}
+			}
+			if (straightRank == 12) {
+				rank = Rank.ROYAL_FLUSH;
+			} else {
+				subRanks = new int[] { straightRank };
+				rank = Rank.STRAIGHT_FLUSH;
+			}
+			return true;
+		}
+	}
+
+	private int findSuit() {
+
 		for (int i = 0; i < suitDis.length; i++) {
 			if (suitDis[i] >= 5) {
 				return i;
 			}
 		}
-		return 0;
+		return -1;
 	}
-	
-	private boolean findStraightFlush(){
-		ArrayList<Integer> rankDisSuit = new ArrayList<Integer>();
-		int count = 0;
-
-		for(int i = 0; i < suitDis.length; i++){
-			if (suitDis[i] >= 5){
-				for (Card c : cards) {
-					if(c.getSuit() == i) {
-						rankDisSuit.add(c.getRank());
-					}
-				}
-				for (int j = rankDisSuit.size(); j >=0; j--) {
-					if (rankDis[i] > 0) {
-						count++;
-						if (count > 4) {
-							return true;
-						}
-			}
-		}
-				
-			}
-			}
-		return false;
-		}
-
-
 
 	private int highestCard() {
-		
-		for(int i = 12; i>=0; i--){
-			if(rankDis[i] > 0){
+
+		for (int i = 12; i >= 0; i--) {
+			if (rankDis[i] > 0) {
 				return i;
 			}
 		}
-		return 0;
+		return -1;
 	}
 
-	//
-	// private void determinRank(int[] suitDis, int[] rankDis) {
-	// if (isRoyalFlush(suitDis, rankDis)) {
-	//
-	// } else {
-	//
-	// }
-	// }
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	// private boolean isStraightFlush(int[] suitDis, int[] rankDis) {
-	// return isFlush(suitDis) && isStraight(rankDis);
-	// }
-	//
-	// private boolean isRoyalFlush
-	
+	public Rank getRank() {
+		return rank;
+	}
 
-
-	private boolean isFlush(int[] suits) {
-		for (int s : suits) {
-			if (s >= 5)
-			return true;
+	private void determineRank() {
+		// find the distributions of cards
+		for (Card c : cards) {
+			suitDis[c.getSuit()]++;
+			rankDis[c.getRank()]++;
 		}
-		return false;
-=======
 
-		determinRank(suitDis, rankDis);
-	}
+		boolean flush = isFlush();
+		boolean straight = isStraight();
+		int maxDup = maxDup();
 
-	private void determinRank(int[] suitDis, int[] rankDis) {
+		if (flush && straight) {
+			if (findStraightFlush()) {
+				return;
+			}
+		}
 
->>>>>>> 22e2043c1d4916581afce0fd4929e6355776cf70
-	}
+		if (maxDup == 4) {
 
-	// private boolean isStraight(int[] ranks) {
-	// for(int s : suits) {
-	// if (s >= 5)
-	// return true;
-	// }
-	// return false;
-	// }
-
-	@Override
-	public int compareTo(Hand h) {
-		// TODO Auto-generated method stub
-		return 0;
+		}
 	}
 
 	public enum Rank {
@@ -208,5 +201,11 @@ public class Hand implements Comparable<Hand> {
 		public int getValue() {
 			return value;
 		}
+	}
+
+	@Override
+	public int compareTo(Hand o) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
