ffd3d39825fa6840be9ccb3ef98f0ab7f93991bb
Nicolai Holst <nicolaiholst@live.dk>
2015-04-23 22:20:26 +0200

    Kneppegodt

--- kode2/JavaPoker/src/neuralnetwork/NN.java
+++ kode2/JavaPoker/src/neuralnetwork/NN.java
@@ -24,26 +24,67 @@ public class NN {
 
 	public NN() {
 		 createNN();
-		 testData("3s 3c", 1);
-//		createNN2();
-//		test2(0.2, 0.8);
-//		test2(0.2, 0.7);
-//		test2(0.1, 0.1);
+		 testData("Ah Ac", 1);
+		 testData("Ah Ac", 4);
 	}
 
 	public void createNN() {
 		// create training set
 		DataSet trainingSet = new DataSet(2, 1);
-		addEntry(trainingSet, "As Ac", 1, 0.85);
-		addEntry(trainingSet, "As Ks", 1, 0.67);
-		addEntry(trainingSet, "As Qh", 1, 0.64);
-		addEntry(trainingSet, "Ks 9s", 1, 0.60);
-		addEntry(trainingSet, "Qs Tc", 1, 0.57);
-		addEntry(trainingSet, "9s 6c", 1, 0.449);
-		addEntry(trainingSet, "8s 8c", 1, 0.69);
-		addEntry(trainingSet, "4s 2c", 1, 0.32);
-		addEntry(trainingSet, "3s 2h", 1, 0.31);
+		addEntry(trainingSet, "As Ac", 2, 0.85);
+		addEntry(trainingSet, "As Ks", 2, 0.67);
+		addEntry(trainingSet, "As Qh", 2, 0.64);
+		addEntry(trainingSet, "Ks 9s", 2, 0.60);
+		addEntry(trainingSet, "Qs Tc", 2, 0.57);
+		addEntry(trainingSet, "9s 6c", 2, 0.44);
+		addEntry(trainingSet, "8s 8c", 2, 0.69);
+		addEntry(trainingSet, "4s 2c", 2, 0.32);
+		addEntry(trainingSet, "3s 2h", 2, 0.31);
+		addEntry(trainingSet, "Js 6s", 2, 0.47);
+		addEntry(trainingSet, "Qs 5c", 2, 0.47);
+		addEntry(trainingSet, "4s 2s", 2, 0.34);
+		addEntry(trainingSet, "5s 2c", 2, 0.31);
+		addEntry(trainingSet, "5s 4h", 2, 0.35);
+		addEntry(trainingSet, "7s 4c", 2, 0.35);
+		addEntry(trainingSet, "8s 4s", 2, 0.40);
+		addEntry(trainingSet, "Ts 2c", 2, 0.39);
 		
+		addEntry(trainingSet, "As Ac", 3, 0.75);
+		addEntry(trainingSet, "As Ks", 3, 0.55);
+		addEntry(trainingSet, "As Qh", 3, 0.53);
+		addEntry(trainingSet, "Ks 9s", 3, 0.50);
+		addEntry(trainingSet, "Qs Tc", 3, 0.47);
+		addEntry(trainingSet, "9s 6c", 3, 0.34);
+		addEntry(trainingSet, "8s 8c", 3, 0.59);
+		addEntry(trainingSet, "4s 2c", 3, 0.22);
+		addEntry(trainingSet, "3s 2h", 3, 0.21);
+		addEntry(trainingSet, "Js 6s", 3, 0.27);
+		addEntry(trainingSet, "Qs 5c", 3, 0.37);
+		addEntry(trainingSet, "4s 2s", 3, 0.24);
+		addEntry(trainingSet, "5s 2c", 3, 0.21);
+		addEntry(trainingSet, "5s 4h", 3, 0.25);
+		addEntry(trainingSet, "7s 4c", 3, 0.25);
+		addEntry(trainingSet, "8s 4s", 3, 0.30);
+		addEntry(trainingSet, "Ts 2c", 3, 0.29);
+		
+		addEntry(trainingSet, "As Ac", 4, 0.65);
+		addEntry(trainingSet, "As Ks", 4, 0.50);
+		addEntry(trainingSet, "As Qh", 4, 0.48);
+		addEntry(trainingSet, "Ks 9s", 4, 0.45);
+		addEntry(trainingSet, "Qs Tc", 4, 0.42);
+		addEntry(trainingSet, "9s 6c", 4, 0.29);
+		addEntry(trainingSet, "8s 8c", 4, 0.54);
+		addEntry(trainingSet, "4s 2c", 4, 0.17);
+		addEntry(trainingSet, "3s 2h", 4, 0.16);
+		addEntry(trainingSet, "Js 6s", 4, 0.22);
+		addEntry(trainingSet, "Qs 5c", 4, 0.32);
+		addEntry(trainingSet, "4s 2s", 4, 0.19);
+		addEntry(trainingSet, "5s 2c", 4, 0.161);
+		addEntry(trainingSet, "5s 4h", 4, 0.20);
+		addEntry(trainingSet, "7s 4c", 4, 0.20);
+		addEntry(trainingSet, "8s 4s", 4, 0.25);
+		addEntry(trainingSet, "Ts 2c", 4, 0.24);
+
 		
 		
 		// create new perceptron network
@@ -56,7 +97,6 @@ public class NN {
 	}
 
 	private void testData(String hand, int opps) {
-		System.out.println("tester");
 		Probability p = ProbabilityCalc.getProbability(
 				Card.createMultiple(hand), null, 1);
 
