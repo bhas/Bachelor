f53e625963e2346786ced2732e529419f6b99e2f
Nicolai Holst <nicolaiholst@live.dk>
2015-05-20 11:05:34 +0200

    no message

--- kode2/JavaPoker/src/testing/MonteCarloTester.java
+++ kode2/JavaPoker/src/testing/MonteCarloTester.java
@@ -1,61 +1,23 @@
 package testing;
 
-import graph.Graph;
-import graph.GraphData;
-import graph.GraphWindow;
 import montecarlo.Probability;
 import montecarlo.ProbabilityCalc;
 
 public class MonteCarloTester {
-	
-//	public static void main(String[] args) {
-//		preflop("Jd Jh");
-//	}
 
-	private static void preflop(String hand) {
-		int calcs = 50;
-		GraphData gd = new GraphData();
-		for (int i = 1; i <= calcs; i++) {
-			Probability p = ProbabilityCalc.getProbability(hand, null, 1);
-			gd.addEntry(i, p.percent());
-			System.out.println(i);
-		}
-		
-		Graph g = new Graph(gd);
-		g.setViewY(0.7, 0.85);
-		g.setViewX(0, calcs);
-		g.drawRanges(true);
-
-		new GraphWindow(g);
-	}
-	
 	public static void main(String[] args) {
-//		long startTime = System.nanoTime();
-//		for(int i = 0; i<100; i++){
-			printProb("Js 8s", 9);
-//			printProb("As Ac", 3);
-//			printProb("As Ac", 4);
-//			printProb("As Ac", 5);
-//			printProb("As Ac", 6);
-//			printProb("As Ac", 7);
-//			printProb("As Ac", 8);
-//			printProb("As Ac", 9);
-			
-//		}
-//		long endTime = System.nanoTime();
-//		double approx = ((endTime-startTime)/1000000000.0);
-//		System.out.println("Took "+ approx/100 + " seconds"); 
+		// Change the values in order to test the different results of the
+		// calculator
+		String communityCards = "Ad 4h Ks";
+		String holeCards = "As 9s";
+		int opponents = 9;
+		printProb(holeCards, communityCards, opponents);
 
 	}
-	
-	private static void printProb(String hand, int opps) {
-		double st = 0.0;
-		for(int i = 0; i<100; i++){
-			Probability prob = ProbabilityCalc.getProbability(hand, null, opps);
-			st += prob.percent();
 
-		}
-		System.out.print(hand + " - ");
-		System.out.println(st/100.0);
+	private static void printProb(String hand, String cc, int opps) {
+		Probability prob = ProbabilityCalc.getProbability(hand, cc, opps);
+		// The result of the calculator
+		System.out.println("result: " + prob.percent());
 	}
 }
---
+++ kode2/Readme.txt
@@ -0,0 +1,9 @@
+//How to run monte carlo
+Edit the main method found in the MonteCarloTester.java file in the testing package
+
+//How to create the neural network
+1. Download neuroph
+2. Import a neural network from /nn/networks
+3. Import a dataset from nn/dataset
+4. Train the network using the dataset
+5. Set input in order to check the output of the network
